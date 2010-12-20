e6449eaf359e68676191827b6d17ee3932bc5d47
Erik Krasner-Karpen <Erik@erik-krasner-karpens-computer.local>
2010-12-19 19:26:10 -0800

    decompiler almost complete

--- decompile_with_library.py
+++ decompile_with_library.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 input_file = open(sys.argv[1], 'r')
 bf = input_file.read()
@@ -8,18 +9,29 @@ output_file.write("from bfcore import *\n")
 output_file.write("a=BFCompiler()\n")
 while(bf != ""):
     output_file.write("a.")
-    if bf[0] == "+":
-        output_file.write("inc()")
-    elif bf[0] == "-":
-        output_file.write("dec()")
-    elif bf[0] == "<":
-        output_file.write("move_left()")
-    elif bf[0] == "<":
-        output_file.write("move_right()")
+    first_char = bf[0]
+    if first_char in "+-<>":
+        match = re.match('\\' + bf[0] + '+', bf)
+        length = len(match)
+        output_file.write(
+                "add" if first_char == "+" else 
+                "sub" if first_char == "-" else
+                "move_left" if first_char == "<" else
+                "move_right"
+                + "(" + length + ")")
+    elif bf[0] == "[":
+        output_file.write("begin_loop()")
+        legnth = 1
+    elif bf[0] == "]":
+        output_file.write("end_loop()")
+        length = 1
     elif bf[0] == ",":
         output_file.write("input_char()")
+        length = 1
     elif bf[0] == ".":
         output_file.write("output_char()")
+        length = 1
+    bf = bf[length:]
     output_file.write("\n")
 output_file.write("print a\n")
 output_file.close()
